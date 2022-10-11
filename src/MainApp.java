b1c52b5546d431172617a8cd4ecbecef08e285e9
Erkan Cihan YILMAZ <therkancihan@gmail.com>
2022-10-11 20:27:52 +0300

    Dünya değişti.

---
+++ src/MainApp.java
@@ -0,0 +1,10 @@
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("Merhaba Dünyalı. Sen dost musun?");
+
+        //TODO: Bu kısımda herhangi bir işlem yapılmayacaktır.
+
+        System.out.println("Merhaba Dünya.!");
+
+    }
+}
